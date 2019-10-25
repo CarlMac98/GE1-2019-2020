f44eb7393fc7f734e7a19768e4115200afb0849d
CarlMac98 <charlyo1998@gmail.com>
2019-10-25 16:30:28 +0100

    ciao

--- GE1Examples2019/Assets/Shooting.cs
+++ GE1Examples2019/Assets/Shooting.cs
@@ -6,6 +6,9 @@ public class Shooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+
+    float ellapsed = float.MaxValue;
+    public int fireRate = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +18,12 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        float toPass = 1.0f / fireRate;
+        ellapsed += Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftControl) && ellapsed > toPass)
         {
             GameObject bullet = GameObject.Instantiate<GameObject>(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            ellapsed = 0;
         }
     }
 }
---
+++ GE1Examples2019/Assets/enemiesSpawn.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemiesSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //int numtanks = 0;
+    private int maxTanks = 5, rng = 10;
+    public GameObject enemy;
+    bool spawning = true;
+    void Start()
+    {
+        IEnumerator spwn = Spawner();
+        StartCoroutine(spwn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int tanks = GameObject.FindGameObjectsWithTag("Tank").Length;
+        if (tanks < maxTanks && spawning == false)
+        {
+            StartCoroutine(Spawner());
+            spawning = false;
+        }
+
+        if (tanks >= maxTanks && spawning == true)
+        {
+            StopCoroutine(Spawner());
+            spawning = false;
+        }
+    }
+
+
+    IEnumerator Spawner()
+    {
+        while (true)
+        {
+            if (GameObject.FindGameObjectsWithTag("Tank").Length < maxTanks)
+            {
+                Vector3 pos = transform.TransformPoint(new Vector3(Random.Range(-rng, rng), 10, Random.Range(-rng, rng)));
+                Quaternion rot = Quaternion.Euler(0, Random.Range(0, 360), 0);
+
+                GameObject enemy = GameObject.Instantiate(this.enemy, pos, rot);
+                enemy.AddComponent<Rigidbody>();
+                enemy.AddComponent<OnHit>();
+            }
+            
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
